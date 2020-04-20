4fc98378e8fbbfa57becdbbfc69dad7d2b1620db
sudarshan <joecurious7@gmail.com>
2020-04-20 00:08:44 -0700

    figuring out hostname conversion to ip address. ipv6 is already basically taken care of in my mind

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
 extern crate icmp;
-extern crate ctrlc;
+extern crate dns_lookup;
+extern crate ipaddress;
+extern crate hostname_validator;
 
 use std::net;
 use std::str::FromStr;
 use std::env;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
 use std::thread;
 use std::time::{Duration, SystemTime};
 use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::icmp::{IcmpPacket, IcmpTypes, IcmpCode, checksum};
 use pnet::packet::icmp::echo_reply::EchoReplyPacket;
 use pnet::packet::Packet;
+use ipaddress::IPAddress;
 
 fn main() {
     let mut packets_lost = 0.0;
     let mut packets_sent = 0.0;
+    // let mut is_ipv4 = false;
+    // let mut is_ipv6 = false;
 
     let args: Vec<String> = env::args().collect();
-    let ip = &args[1];
-    let ip_address = match net::IpAddr::from_str(ip) {
+    let arg = &args[1];
+    let mut ip = String::from(arg);
+    let temp = match IPAddress::parse(arg) {
+        Ok(addr) => addr,
+        Err(error) => {
+            println!("might be hostname");
+        },
+    };
+
+
+    if temp.is_ipv4() {
+        println!("is ipv4");
+    } else if temp.is_ipv6() {
+        println!("is ipv6");
+    } else {
+        println!("might be hostname");
+        let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
+        ip = ips[0].to_string();
+        let t = IPAddress::parse(ip.to_string()).unwrap();
+    }
+    // let is_hostname = arg.chars().any(char::is_alphabetic);
+    // if is_hostname {
+    //     let ips: Vec<std::net::IpAddr> = lookup_host(arg).unwrap();
+    //     ip = ips[0].to_string();
+    //     println!("{}", ip);
+    // }
+
+    let ip_address = match net::IpAddr::from_str(&ip) {
         Ok(addr) => addr,
         Err(error) => {
             panic!("Problem creating the IP address: {:?}", error)
@@ -40,11 +69,12 @@ fn main() {
         ping.send(&buf[..]);
         packets_sent += 1.0;
         ping.recv(&mut buf[..]);
+        println!("made it to here");
         let elapsed = match now.elapsed() {
             Ok(elapsed) => elapsed.as_millis(),
             Err(_error) => 0,
         };
-        if let Some(icmp_packet) = EchoReplyPacket::new(&buf[20..]) {
+        if let Some(_icmp_packet) = EchoReplyPacket::new(&buf[20..]) {
             println!("reply from {}: icmp_seq={} time={}ms packet_loss={}%",
             ip_address.to_string(), sequence_number, elapsed, packets_lost / packets_sent * 100.0);
         } else {
