a7685cb167eef75b4d3c2a99e8e8e003103028ed
sudarshan <joecurious7@gmail.com>
2020-04-20 00:51:57 -0700

    slogging through

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate dns_lookup;
 extern crate ipaddress;
 extern crate hostname_validator;
 
-use std::net;
+use std::net::{IpAddr, Ipv4Addr};
 use std::str::FromStr;
 use std::env;
 use std::thread;
@@ -17,43 +17,64 @@ use ipaddress::IPAddress;
 fn main() {
     let mut packets_lost = 0.0;
     let mut packets_sent = 0.0;
-    // let mut is_ipv4 = false;
-    // let mut is_ipv6 = false;
 
     let args: Vec<String> = env::args().collect();
     let arg = &args[1];
     let mut ip = String::from(arg);
-    let temp = match IPAddress::parse(arg) {
-        Ok(addr) => addr,
-        Err(error) => {
-            println!("might be hostname");
-        },
-    };
-
-
-    if temp.is_ipv4() {
-        println!("is ipv4");
-    } else if temp.is_ipv6() {
-        println!("is ipv6");
+    let mut maybe_ipv6 = false;
+    let mut maybe_hostname = false;
+    let mut maybe_ipv4 = false;
+    if ip.contains(":") {
+        // this is maybe ipv6
+        maybe_ipv6 = true;
+    } else if arg.chars().any(char::is_alphabetic) {
+        // might be a hostname
+        maybe_hostname = true;
     } else {
-        println!("might be hostname");
-        let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
-        ip = ips[0].to_string();
-        let t = IPAddress::parse(ip.to_string()).unwrap();
+        maybe_ipv4 = true;
     }
+    // if maybe_hostname {
+    //
+    // }
     // let is_hostname = arg.chars().any(char::is_alphabetic);
     // if is_hostname {
     //     let ips: Vec<std::net::IpAddr> = lookup_host(arg).unwrap();
     //     ip = ips[0].to_string();
     //     println!("{}", ip);
     // }
+    let mut temp = None;
+    let mut ip_address = None;
+    // let temp = IPAddress::parse(arg).unwrap();
+    if maybe_hostname {
+        println!("might be hostname");
+        let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
+        ip = ips[0].to_string();
+        temp = Some(IPAddress::parse(ip.to_string()).unwrap());
+    } else {
+        temp = Some(IPAddress::parse(arg).unwrap());
+        if Some(temp).is_ipv4() {
+            ip_address = Some(IpAddr::V4(Ipv4Addr::from_str(&arg)));
+        }
+    }
 
-    let ip_address = match net::IpAddr::from_str(&ip) {
-        Ok(addr) => addr,
-        Err(error) => {
-            panic!("Problem creating the IP address: {:?}", error)
-        },
-    };
+
+    // if temp.is_ipv4() {
+    //     println!("is ipv4");
+    // } else if temp.is_ipv6() {
+    //     println!("is ipv6");
+    // } else {
+    //     println!("might be hostname");
+    //     let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
+    //     ip = ips[0].to_string();
+    //     temp = Some(IPAddress::parse(ip.to_string()).unwrap());
+    // }
+    //
+    // let ip_address = match net::IpAddr::from_str(&ip) {
+    //     Ok(addr) => addr,
+    //     Err(error) => {
+    //         panic!("Problem creating the IP address: {:?}", error)
+    //     },
+    // };
 
     let ping = icmp::IcmpSocket::connect(ip_address);
     let mut ping = ping.unwrap();
