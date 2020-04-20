2dbca79494dd1bb52688aabf3a87cbf048762745
sudarshan <joecurious7@gmail.com>
2020-04-20 11:02:51 -0700

    finished basic functionality

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate dns_lookup;
 extern crate ipaddress;
 extern crate hostname_validator;
 
-use std::net::{IpAddr, Ipv4Addr};
-use std::str::FromStr;
+use std::net::{IpAddr};
 use std::env;
 use std::thread;
 use std::time::{Duration, SystemTime};
@@ -12,7 +11,6 @@ use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::icmp::{IcmpPacket, IcmpTypes, IcmpCode, checksum};
 use pnet::packet::icmp::echo_reply::EchoReplyPacket;
 use pnet::packet::Packet;
-use ipaddress::IPAddress;
 
 fn main() {
     let mut packets_lost = 0.0;
@@ -20,63 +18,23 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     let arg = &args[1];
-    let mut ip = String::from(arg);
-    let mut maybe_ipv6 = false;
-    let mut maybe_hostname = false;
-    let mut maybe_ipv4 = false;
-    if ip.contains(":") {
-        // this is maybe ipv6
-        maybe_ipv6 = true;
-    } else if arg.chars().any(char::is_alphabetic) {
-        // might be a hostname
-        maybe_hostname = true;
-    } else {
-        maybe_ipv4 = true;
-    }
-    // if maybe_hostname {
-    //
-    // }
-    // let is_hostname = arg.chars().any(char::is_alphabetic);
-    // if is_hostname {
-    //     let ips: Vec<std::net::IpAddr> = lookup_host(arg).unwrap();
-    //     ip = ips[0].to_string();
-    //     println!("{}", ip);
-    // }
-    let mut temp = None;
-    let mut ip_address = None;
-    // let temp = IPAddress::parse(arg).unwrap();
-    if maybe_hostname {
-        println!("might be hostname");
-        let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
-        ip = ips[0].to_string();
-        temp = Some(IPAddress::parse(ip.to_string()).unwrap());
-    } else {
-        temp = Some(IPAddress::parse(arg).unwrap());
-        if Some(temp).is_ipv4() {
-            ip_address = Some(IpAddr::V4(Ipv4Addr::from_str(&arg)));
+    let ip = String::from(arg);
+
+    let mut ip_address: Option<IpAddr> = None;
+
+    if arg.chars().any(char::is_alphabetic) {
+        let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(&arg).unwrap();
+        for addr in ips.iter() {
+            if addr.is_ipv4() {
+                ip_address = Some(*addr);
+            }
         }
+    } else {
+        ip_address = Some(ip.parse().unwrap());
     }
 
 
-    // if temp.is_ipv4() {
-    //     println!("is ipv4");
-    // } else if temp.is_ipv6() {
-    //     println!("is ipv6");
-    // } else {
-    //     println!("might be hostname");
-    //     let ips: Vec<std::net::IpAddr> = dns_lookup::lookup_host(arg).unwrap();
-    //     ip = ips[0].to_string();
-    //     temp = Some(IPAddress::parse(ip.to_string()).unwrap());
-    // }
-    //
-    // let ip_address = match net::IpAddr::from_str(&ip) {
-    //     Ok(addr) => addr,
-    //     Err(error) => {
-    //         panic!("Problem creating the IP address: {:?}", error)
-    //     },
-    // };
-
-    let ping = icmp::IcmpSocket::connect(ip_address);
+    let ping = icmp::IcmpSocket::connect(ip_address.unwrap());
     let mut ping = ping.unwrap();
     let packet_size = 64; // arbitrary
     let mut sequence_number: u16 = 0;
@@ -87,21 +45,20 @@ fn main() {
 
         make_packet(&sequence_number, &identifier, &mut buf[..]);
         let now = SystemTime::now();
-        ping.send(&buf[..]);
+        ping.send(&buf[..]).unwrap();
         packets_sent += 1.0;
-        ping.recv(&mut buf[..]);
-        println!("made it to here");
+        ping.recv(&mut buf[..]).unwrap();
         let elapsed = match now.elapsed() {
             Ok(elapsed) => elapsed.as_millis(),
             Err(_error) => 0,
         };
         if let Some(_icmp_packet) = EchoReplyPacket::new(&buf[20..]) {
+            let ip_address = ip_address.unwrap();
             println!("reply from {}: icmp_seq={} time={}ms packet_loss={}%",
             ip_address.to_string(), sequence_number, elapsed, packets_lost / packets_sent * 100.0);
         } else {
             packets_lost += 1.0;
         }
-        println!("success!");
         thread::sleep(Duration::new(1, 0));
         sequence_number += 1;
     }
